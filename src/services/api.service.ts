9702f153ddd1041a01f0d5932ecce8baa80e29e7
Alexander Quatrini <alexanderquatrini@gmail.com>
2022-10-12 09:53:18 -0500

    Adds api call

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
+import { IRobotResponse } from 'src/models/IRobotResponse.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
 
-  constructor() { }
+  private API_URL: string = "https://60c8ed887dafc90017ffbd56.mockapi.io/robots";
+
+  constructor(private http: HttpClient) { }
+
+  retrieveAPIResponse(): Observable<IRobotResponse>{
+       const obs = this.http.get<IRobotResponse>(this.API_URL);
+       return obs;
+  }
+
 }
