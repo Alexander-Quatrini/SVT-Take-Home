5f69d376eb3de6dfbe082e63f41ecdded5e5bd0b
Alexander Quatrini <alexanderquatrini@gmail.com>
2022-10-12 10:56:58 -0500

    Moves api call to function, multiple tries allowed

--- src/robot-table/robot-table.component.ts
+++ src/robot-table/robot-table.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { Robot } from 'src/models/IRobotResponse.model';
@@ -13,14 +14,33 @@ export class RobotTableComponent implements OnInit {
   constructor(private apiService: APIService) { }
 
   robots: Robot[] = [];
+  failed: boolean = false;
+  loading: boolean = true;
+  counter: number = 0;
+
 
   ngOnInit(): void {
+    while(this.robots.length == 0 && this.counter < 3){
+      this.retrieveRobotInfo();
+      this.counter++;
+    }
+  }
+
+  retrieveRobotInfo(): void{
     firstValueFrom(this.apiService.retrieveAPIResponse()).then(
       data => {
         this.robots = data;
         console.log(this.robots);
+        this.failed = false;
+        this.loading = false;
+        console.log(this.loading);
       }
-    )
+    ).catch(err => {
+      const error = err as HttpErrorResponse;
+      console.log(error.message);
+      this.failed = true;
+      this.loading = false;
+    })
   }
 
 }
