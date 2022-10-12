b055df3c99868cd153d444851527936f72ded819
Alexander Quatrini <alexanderquatrini@gmail.com>
2022-10-12 09:58:05 -0500

    Creates robot table component, adds logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { APIService } from 'src/services/api.service';
+import { RobotTableComponent } from '../robot-table/robot-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RobotTableComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [ APIService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
