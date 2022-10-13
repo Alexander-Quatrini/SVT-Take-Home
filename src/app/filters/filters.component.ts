14d97d792ee5d47af6e8559aa00edf6f30649621
Alexander Quatrini <alexanderquatrini@gmail.com>
2022-10-13 09:27:49 -0500

    Hide tooltips when slider is inactive

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,10 +1,12 @@
+import { ViewEncapsulation } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import  noUiSlider, { PipsMode } from 'noUiSlider';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.css']
+  styleUrls: ['./filters.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FiltersComponent implements OnInit {
 
@@ -22,6 +24,7 @@ export class FiltersComponent implements OnInit {
           'min': 1,
           'max': 100
       },
+      tooltips: true,
       format: {
         from: (num) => Number.parseFloat(num),
         to: (number) => Math.round(number),
